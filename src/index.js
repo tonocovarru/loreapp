29ce37620cd42eaf7c82322fd8a931c134ac39cf
tonocovarru <joseantonio.covarrubias@gmail.com>
2019-07-14 15:44:49 -0500

    Commit with creative componentes

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
+
+import AuthLayout from "layouts/Auth.jsx";
+//import RtlLayout from "layouts/RTL.jsx";
+//import AdminLayout from "layouts/Admin.jsx";
+
+import "assets/scss/material-dashboard-pro-react.scss?v=1.7.0";
+
+const hist = createBrowserHistory();
+
+ReactDOM.render(
+  <Router history={hist}>
+    <Switch>
+      <Route path="/auth" component={AuthLayout} />
+      <Redirect from="/" to="/auth" />
+    </Switch>
+  </Router>,
+  document.getElementById("root")
+);
+
+//      <Route path="/rtl" component={RtlLayout} />
+//<Route path="/admin" component={AdminLayout} />
+//      <Redirect from="/" to="/admin/dashboard" />
